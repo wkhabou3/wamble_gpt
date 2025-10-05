9cb0f28f91099c113dfd5747d0d370c19aace514
wkhabou3 <wkhabou33@gmail.com>
2025-10-04 21:30:02 -0400

    Implemented 2-tuple Markov model

--- wamgpt.py
+++ wamgpt.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # constants
 # TODO: There are special combinations of characters like ellipsiseses. What's the best way to track these
-punctuation = ['.', '!', '?']
+punctuation_types = ['.', '!', '?']
 punctuation_ignore = ['\"', '\'']
 sentence_length = 4
 
@@ -22,6 +22,18 @@ def rollDice(word):
         currWeight = markovModel[word][i]
         if currWeight > dice:
             return i
+        else:
+            dice -= currWeight
+
+# rollDice, but with 2-tuple markov model.
+def rollDiceTuple(prevWord, currWord):
+    currPhrase = (prevWord, currWord)
+    sumWeights = tupleWeights[currPhrase]
+    dice = random.random() * sumWeights
+    for i in markovTuple[currPhrase]:
+        currWeight = markovTuple[currPhrase][i]
+        if currWeight > dice:
+            return currWord, i
         else:
             dice -= currWeight
 
@@ -64,9 +76,21 @@ allContent = []
 #    print('Gimme another file or enter -1 to continue to wamble-gpt')
 #    filename = input()
 
-filename = 'test_files/independence.txt'
-fileContent = open(filename, "r").read()
-allContent.append(fileContent)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+input_files_dir = os.path.join(script_dir, 'test_files')
+
+if os.path.exists(input_files_dir):
+    for filename in os.listdir(input_files_dir):
+        if filename.endswith('.txt'):
+            filepath = f'{input_files_dir}/{filename}'
+            try:
+                fileContent = open(filepath, "r").read()
+                allContent.append(fileContent)
+                print(f'Loaded: {filename}')
+            except Exception as e:
+                print(f'Error reading {filename}: {e}')
+else:
+    print(f'Directory {input_files_dir} was not found')
 
 # split each file's text into words delimited by spaces
 # TODO: generate POS tags for all words
@@ -83,7 +107,7 @@ for i in allWords:
     for j in i:
         if j[-1] in punctuation_ignore:
             j = j[0:-1]
-        if j[-1] in punctuation:
+        if j[-1] in punctuation_types:
             currPunctuated.append(j[0:-1])
             currPunctuated.append(j[-1])
             continue
@@ -93,7 +117,6 @@ for i in allWords:
 del allWords
 
 # Parse all words into Markov model, including sentence starters and enders
-# TODO: Create Markov models for n-tuples of words (just start with 2-tuples for now)
 markovModel = {}
 sentenceStarters = {}
 sentenceEnders = {}
@@ -107,28 +130,25 @@ for i in allPunctuated:
         currWord = i[j]
         nextWord = i[j + 1]
         # Case 1: current word is a punctuation mark. Do nothing
-        if currWord in punctuation:
+        if currWord in punctuation_types:
             continue
         # Case 2: current word comes before a punctuation. Then:
         # - add word as a key of sentenceEnders. increase weight of corresponding punctuation by 1
         # - add 1 to total weight of all punctuation associated with the word
-        elif nextWord in punctuation:
+        elif nextWord in punctuation_types:
             if currWord in sentenceEnders:
                 if nextWord in sentenceEnders[currWord]:
                     sentenceEnders[currWord][nextWord] += 1
                 else:
                     sentenceEnders[currWord][nextWord] = 1
-            else:
-                sentenceEnders[currWord] = {nextWord: 1}
-            if currWord in enderWeights:
                 enderWeights[currWord] += 1
             else:
+                sentenceEnders[currWord] = {nextWord: 1}
                 enderWeights[currWord] = 1
-            continue
         # Case 3: current word comes after punctuation. Then:
         # - add word as a key of sentenceStarters. Increase its weight by 1
         # - add 1 to sum total weight of all sentence starters
-        elif i[j - 1] in punctuation:
+        elif i[j - 1] in punctuation_types:
             if currWord in sentenceStarters:
                 sentenceStarters[currWord] += 1
             else:
@@ -143,27 +163,49 @@ for i in allPunctuated:
                 markovModel[currWord][nextWord] += 1
             else:
                 markovModel[currWord][nextWord] = 1
-        else:
-            markovModel[currWord] = {nextWord: 1}
-        if currWord in weights:
             weights[currWord] += 1
         else:
+            markovModel[currWord] = {nextWord: 1}
             weights[currWord] = 1
+
+# We are making an n-tuple markov model, {(word1, word2): {word3, weight}}
+# We have to keep track of prevWord when generating to add this functionality
+# Use punctuation as tokens in this process.
+markovTuple = {}
+tupleWeights = {}
+for i in allPunctuated:
+    for j in range(1, len(i) - 1):
+        currWord = i[j]
+        prevWord = i[j - 1]
+        nextWord = i[j + 1]
+        currPhrase = (prevWord, currWord)
+        if (currPhrase in markovTuple):
+            if nextWord in markovTuple[currPhrase]:
+                markovTuple[currPhrase][nextWord] += 1
+            else:
+                markovTuple[currPhrase][nextWord] = 1
+            tupleWeights[currPhrase] += 1
+        else:
+            markovTuple[currPhrase] = {nextWord: 1}
+            tupleWeights[currPhrase] = 1
 
 # Use the dict to generate sentences with the following rules:
 # When starting a sentence, roll dice to choose a word from sentenceStarters.
 # If some word is a sentence ender, roll dice to decide whether to end the sentence there. Roll also influenced by current sentence length
-# If for some reaosn word is not in the Markov model, just end the sentence.
+# If for some reason word is not in the Markov model, just end the sentence.
 # For all other words, roll dice to decide the next word using Markov model.
+# Use 2-tuple markov model unless key not found.
 print('How many words you want')
 numWords = int(input())
 output = ''
 currWordCount = 0
 currWord = ''
+prevWord = ''
 
 for i in range(numWords):
     # Starting a sentence
     if currWordCount == 0:
+        prevWord = currWord
         currWord = rollDiceStarter()
         output += ' ' + currWord
         currWordCount += 1
@@ -171,6 +213,7 @@ for i in range(numWords):
         continue
     # Finding a sentence ender
     if currWord in sentenceEnders:
+        prevWord = currWord
         endSentence, punctuation = rollDiceEnder(currWord, currWordCount)
         if (not endSentence) and (currWord in markovModel):
             currWord = rollDice(currWord)
@@ -179,6 +222,7 @@ for i in range(numWords):
             if (not punctuation):
                 punctuation = '.'
             output += punctuation
+            currWord = punctuation
             currWordCount = 0
         continue
     # alien :O
@@ -186,7 +230,17 @@ for i in range(numWords):
         output += '.'
         currWordCount = 0
         continue
-    # everything else
+    # 2-tuple Markov chain
+    if (prevWord, currWord) in markovTuple:
+        prevWord, currWord = rollDiceTuple(prevWord, currWord)
+        output += ' ' + currWord
+        if currWord in punctuation_types:
+            currWordCount = 0
+        else:
+            currWordCount += 1
+        continue
+    # Everything else
+    prevWord = currWord
     currWord = rollDice(currWord)
     output += ' ' + currWord
     currWordCount += 1
